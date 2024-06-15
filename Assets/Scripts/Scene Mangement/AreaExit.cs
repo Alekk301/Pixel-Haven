e827c4f51a405f4f592c73785569b04f59714535
Alekk301 <ducanhthealex209@gmail.com>
2024-06-15 17:03:16 +0700

    sceneEntruy

--- Assets/Scripts/Scene Mangement/AreaExit.cs
+++ Assets/Scripts/Scene Mangement/AreaExit.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<PlayerController>())
